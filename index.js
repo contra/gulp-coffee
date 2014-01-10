a456e573ccacbb0bb7ba4c39e4cbb8b7ef2d5960
Steven Luscher <github@steveluscher.com>
2014-01-10 14:51:06 -0800

    Added an ignore option. You can specify a glob pattern of files to pass through untouched. `*.js` is the default.

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ var coffee = require('coffee-script');
 var gutil = require('gulp-util');
 var formatError = require('./lib/formatError');
 var Buffer = require('buffer').Buffer;
+var match = require('minimatch');
+var path = require('path');
 var defaults = require('defaults');
 
 module.exports = function(opt){
   var options = defaults(opt, {
+    ignore: '*.js',
     bare: false,
     literate: false,
     sourceMap: false
   });
 
   function modifyFile(file){
-    if (file.isNull()) return this.emit('data', file); // pass along
+    if (file.isNull() || match(path.basename(file.path), options.ignore)) return this.emit('data', file); // pass along
     if (file.isStream()) return this.emit('error', new Error("gulp-coffee: Streaming not supported"));
 
     var str = file.contents.toString('utf8');
